6613b3ef4c4376918b9f72af81ca6b934c4959a4
Steve Boyd <emteknetnz@gmail.com>
2023-07-17 19:09:30 +1200

    .

--- run.php
+++ run.php
@@ -5,26 +5,20 @@ include 'funcs.php';
 // global variable
 $MODULE_DIR = '';
 
-function run()
-{
-    global $MODULE_DIR;
+$scriptFiles = array_merge(
+    getScriptFiles('any'),
+    // @todo detect cms major to use based on command line args
+    // works out default branch // default major // diff - see gha-merge-up
+    getScriptFiles('5'),
+);
 
-    $scriptFiles = array_merge(
-        getScriptFiles('any'),
-        // @todo detect cms major to use based on command line args
-        // works out default branch // default major // diff - see gha-merge-up
-        getScriptFiles('5'),
-    );
-
-    // @todo - get modules from  funcs.php::getSupportedModules($cmsMajor)
-    $modules = [
-        'silverstripe-config'
-    ];
-    foreach ($modules as $module) {
-        $MODULE_DIR = "_modules/$module";
-        foreach ($scriptFiles as $scriptFile) {
-            include $scriptFile;
-        }
+// @todo - get modules from  funcs.php::getSupportedModules($cmsMajor)
+$modules = [
+    'silverstripe-config'
+];
+foreach ($modules as $module) {
+    $MODULE_DIR = "_modules/$module";
+    foreach ($scriptFiles as $scriptFile) {
+        include $scriptFile;
     }
 }
-run();
--- scripts/cms5/composer.php
+++ scripts/cms5/composer.php
@@ -1,28 +1,24 @@
 <?php
 
-if (!function_exists('cms5Composer')) {
-    function cms5Composer()
-    {
-        global $MODULE_DIR;
-        $filename = "$MODULE_DIR/composer.json";
-        if (!file_exists($filename)) {
-            info("No composer.json found in $MODULE_DIR though that's probably OK");
-            return;
-        }
-        $json = json_decode(file_get_contents($filename), true);
-        if (!$json) {
-            error("Failed to parse json in $filename");
-        }
-        $version = $json['require-dev']['phpunit/phpunit'] ?? null;
-        if (is_null($version)) {
-            return;
-        }
-        if ($version !== '^9.5') {
-            return;
-        }
-        $json['require-dev']['phpunit/phpunit'] = '^9.6';
-        $flags = JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE;
-        writeFile($filename, json_encode($json, $flags));
+(function() {
+    global $MODULE_DIR;
+    $filename = "$MODULE_DIR/composer.json";
+    if (!file_exists($filename)) {
+        info("No composer.json found in $MODULE_DIR though that's probably OK");
+        return;
     }
-}
-cms5Composer();
+    $json = json_decode(file_get_contents($filename), true);
+    if (!$json) {
+        error("Failed to parse json in $filename");
+    }
+    $version = $json['require-dev']['phpunit/phpunit'] ?? null;
+    if (is_null($version)) {
+        return;
+    }
+    if ($version !== '^9.5') {
+        return;
+    }
+    $json['require-dev']['phpunit/phpunit'] = '^9.6';
+    $flags = JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE;
+    writeFile($filename, json_encode($json, $flags));
+})();
