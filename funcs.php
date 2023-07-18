135e408da47a440ccc01c605db0c16f9455be582
Steve Boyd <emteknetnz@gmail.com>
2023-07-18 12:57:46 +1200

    .

--- funcs.php
+++ funcs.php
@@ -3,6 +3,8 @@
 use Symfony\Component\Console\Output\NullOutput;
 use Symfony\Component\Console\Input\ArgvInput;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
 
 function getScriptFiles($cmsMajor)
 {
@@ -26,14 +28,12 @@ function getScriptFiles($cmsMajor)
 function writeTemplateFileEvenIfExists($filename, $content)
 {
     global $MODULE_DIR;
-    $content = prepareContent($content);
     writeFile("$MODULE_DIR/$filename", $content);
 }
 
 function writeTemplateFileIfNotExists($filename, $content)
 {
     global $MODULE_DIR;
-    $content = prepareContent($content);
     if (!file_exists("$MODULE_DIR/$filename")) {
         writeFile("$MODULE_DIR/$filename", $content);
     }
@@ -51,21 +51,21 @@ function getSupportedModules($cmsMajor)
     $json = json_decode(file_get_contents($filename), true);
     $modules = [];
     foreach ($json as $module) {
+        $ghrepo = $module['github'];
         $modules[] = [
-            'cloneUrl' => 'git@github.com:' . $module['github'] . '.git',
+            'ghrepo' => $ghrepo,
+            'account' => explode('/', $ghrepo)[0],
+            'repo' => explode('/', $ghrepo)[1],
+            'cloneUrl' => "git@github.com:$ghrepo.git",
             'branch' => max($module['branches'])
         ];
     }
     return $modules;
 }
 
-function prepareContent($content)
-{
-    return trim($content) . "\n";
-}
-
 function writeFile($filename, $contents)
 {
+    $contents = trim($contents) . "\n";
     file_put_contents($filename, $contents);
     info("Wrote to $filename");
 }
@@ -83,8 +83,22 @@ function warning($message)
 
 function error($message)
 {
-    // don't throw hard exception here, instead let the rest of this script run
     getIo()->error($message);
+    outputPullRequestsCreated();
+    die;
+}
+
+function cmd($cmd, $cwd)
+{
+    $message = "Running command: $cmd in $cwd";
+    info($message);
+    // using Process::fromShellCommandline() instead of new Process() so that pipes work
+    $process = Process::fromShellCommandline($cmd, $cwd);
+    $process->run();
+    if (!$process->isSuccessful()) {
+        error($process->getErrorOutput());
+    }
+    return trim($process->getOutput());
 }
 
 function getIo(): SymfonyStyle
@@ -100,4 +114,48 @@ function removeDir($dirname)
     }
     info("Removing $dirname");
     shell_exec("rm -rf $dirname");
+}
+
+function validateSystem()
+{
+    if (!githubToken()) {
+        error('Could not get github token');
+    }
+}
+
+function githubToken()
+{
+    // using composer token that's assumed to be on users laptop
+    // could also support passing in a token via env variable, though this should be good enough
+    $auth = cmd('cat ~/.config/composer/auth.json', '.');
+    if (!$auth) {
+        return '';
+    }
+    $json = json_decode($auth, true);
+    return $json['github-oauth']['github.com'] ?? '';
+}
+
+function curlPost($url, $data, $headers = [])
+{
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    $response = curl_exec($ch);
+    $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+    return [$response, $httpcode];
+}
+
+function outputPullRequestsCreated()
+{
+    global $PULL_REQUESTS_CREATED;
+    $io = getIo();
+    $io->writeln('');
+    $io->writeln('Pull requests created:');
+    foreach ($PULL_REQUESTS_CREATED as $pr) {
+        $io->writeln($pr);
+    }
+    $io->writeln('');
 }
--- run.php
+++ run.php
@@ -12,6 +12,7 @@ use Symfony\Component\Console\Input\InputOption;
 
 // global variables
 $MODULE_DIR = '';
+$PULL_REQUESTS_CREATED = [];
 $OUT = null;
 
 $app = new Application();
@@ -23,13 +24,21 @@ $app->register('update')
     EOT))
     ->addOption('reset', 'r', InputOption::VALUE_NONE, 'Delete _data and _modules dirs')
     ->addOption('dry-run', 'd', InputOption::VALUE_NONE, 'Dry-run - do not create pull-requests')
-    ->addOption('module', 'm', InputOption::VALUE_REQUIRED, 'Only update the specified module e.g. silverstripe-config')
+    ->addOption('account', '-a', InputOption::VALUE_REQUIRED, 'Account to use for pull-requests (default: creative-commoners)')
+    ->addOption('modules', 'm', InputOption::VALUE_REQUIRED, 'Only update the specified modules (without account prefix) separated by commas e.g. silverstripe-config,silverstripe-assets')
     ->setCode(function (InputInterface $input, OutputInterface $output): int {
+
         // variables
-        global $MODULE_DIR, $OUT;
+        global $MODULE_DIR, $OUT, $PULL_REQUESTS_CREATED;
         $OUT = $output;
         $dataDir = '_data';
         $modulesDir = '_modules';
+        $prTitle = 'MNT Run module-standardiser';
+        $toolUrl = 'https://github.com/silverstripe/module-standardiser';
+        $prDescripton = "Created using [$toolUrl]($toolUrl)";
+
+        // make sure everything setup correctly
+        validateSystem();
 
         // branch
         $branch = $input->getOption('branch');
@@ -56,32 +65,111 @@ $app->register('update')
 
         // modules
         $modules = getSupportedModules($cmsMajor);
-        if ($input->getOption('module')) {
-            $modules = array_filter($modules, function ($module) use ($input) {
-                return $module === $input->getOption('module');
+        if ($input->getOption('modules')) {
+            $moduleArray = explode(',', $input->getOption('modules'));
+            $modules = array_filter($modules, function ($module) use ($input, $moduleArray) {
+                return in_array($module['repo'], $moduleArray);
             });
         }
-        exit;
 
         // script files
         $scriptFiles = array_merge(
             getScriptFiles('any'),
             getScriptFiles($cmsMajor),
         );
+
+        // clone repos & run scripts
         foreach ($modules as $module) {
-            $MODULE_DIR = "$modulesDir/$module";
+            $account = $module['account'];
+            $repo = $module['repo'];
+            $cloneUrl = $module['cloneUrl'];
+            $MODULE_DIR = "$modulesDir/$repo";
             if (!file_exists($MODULE_DIR)) {
-                $cmd = "git clone {$module['cloneUrl']} $MODULE_DIR";
-                $output->writeln($cmd);
-                shell_exec($cmd);
+                cmd("git clone $cloneUrl", $modulesDir);
             }
+            // work out module branch to checkout
+            if ($branch === 'next-patch') {
+                // @todo
+                error('branch=next-patch is not supported yet');
+                $checkoutBranch = 'todo';
+            } elseif ($branch === 'last-major-next-patch') {
+                // @todo
+                error('branch=last-major-next-patch is not supported yet');
+                $checkoutBranch = 'todo';
+            } else {
+                // next-minor (default)
+                // assuming that default branch is the next-minor for now
+                // @todo: don't do this
+                $cmd = "git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@'";
+                $checkoutBranch = cmd($cmd, $MODULE_DIR);
+            }
+
+            // checkout the base branch - this is important if not using the --reset option
+            cmd("git checkout $checkoutBranch", $MODULE_DIR);
+            $timestamp = time();
+
+            // create a new branch used for the pull-request
+            $prBranch = "pulls/$checkoutBranch/module-standardiser-$timestamp";
+            cmd("git checkout -b $prBranch", $MODULE_DIR);
+
+            // run scripts on $MODULE_DIR
             foreach ($scriptFiles as $scriptFile) {
                 include $scriptFile;
             }
-        }
 
-        // return status
+            // set git remote
+            $prAccount = $input->getOption('account') ?? 'creative-commoners';
+            $origin = cmd('git remote get-url origin', $MODULE_DIR);
+            $prOrigin = str_replace("git@github.com:$account", "git@github.com:$prAccount", $origin);
+            // remove any existing pr-remote - need to do this in case we change the account option
+            if (cmd('git remote | grep pr-remote', $MODULE_DIR)) {
+                cmd('git remote remove pr-remote', $MODULE_DIR);
+            }
+            cmd("git remote add pr-remote $prOrigin", $MODULE_DIR);
+
+            // commit changes, push changes and create pull-request
+            $status = cmd('git status', $MODULE_DIR);
+            if (strpos($status, 'nothing to commit') !== false) {
+                info("No changes to commit for $repo");
+            } else {
+                cmd('git add .', $MODULE_DIR);
+                cmd("git commit -m '$prTitle'", $MODULE_DIR);
+                if ($input->getOption('dry-run')) {
+                    info('Not pushing changes or creating pull-request because --dry-run option is set');
+                } else {
+                    // push changes to pr-remote
+                    cmd("git push -u pr-remote $prBranch", $MODULE_DIR);
+                    // create pull-request using github api
+                    // https://docs.github.com/en/rest/pulls/pulls?apiVersion=2022-11-28#create-a-pull-request
+                    $url = "https://api.github.com/repos/$account/$repo/pulls";
+                    $data = json_encode([
+                        'title' => $prTitle,
+                        'body' => $prDescripton,
+                        'head' => "$prAccount:$prBranch",
+                        'base' => $checkoutBranch,
+                    ], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+                    $token = githubToken();
+                    $headers = [
+                        'User-Agent: silverstripe-module-standardiser',
+                        'Accept: application/vnd.github+json',
+                        "Authorization: Bearer $token",
+                        'X-GitHub-Api-Version: 2022-11-28'
+                    ];
+                    list($response, $httpcode) = curlPost($url, $data, $headers);
+                    if ($httpcode === 201) {
+                        $json = json_decode($response, true);
+                        $prUrl = $json['html_url'];
+                        $PULL_REQUESTS_CREATED[] = $prUrl;
+                        info("Created pull-request for $repo");
+                    } else {
+                        warning("HTTP code $httpcode returned from github api");
+                        warning($response);
+                        error("Failed to create pull-request for $repo");
+                    }
+                }
+            }
+        }
+        outputPullRequestsCreated();
         return Command::SUCCESS;
     });
 $app->run();
-
